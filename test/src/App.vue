30d638732c6c004784bb1949de14658da877673a
bright <2207244732@qq.com>
2020-12-29 11:30:32 +0800

    init vue3

---
+++ test/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="app">
+       <p>{{person}}说</p>
+        <button
+        @click="add"
+        >
+          我被点击了{{count}}次
+        </button>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  /*
+  data(){
+    return{   
+      person:'Tom',
+      count:1
+    } 
+  },
+  methods:{
+    add(){
+      this.count+=1
+    }
+  }
+  */
+  setup(){//组合API的入口
+  
+  }
+}
+</script>
