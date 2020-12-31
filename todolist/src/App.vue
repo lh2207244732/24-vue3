c6a045157283ef94d8ce8c707319d435b35a9fe2
bright <2207244732@qq.com>
2020-12-31 08:54:27 +0800

    add

---
+++ todolist/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
---
+++ todolist/src/pages/todolist/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="todolist">
+        <div class="head">
+            <input type="text" v-model='task' />
+            <button @click="addItem">提交</button>
+        </div>
+        <transition-group  class="list" tag='ul' name="fade" appear>
+            <li 
+                class="item"
+                v-for="item in list"
+                :key="item.id"
+                @click="delItem(item.id)"
+            >
+                {{item.task}}
+            </li>
+        </transition-group>
+        <p class="footer">共计{{total}}个项目</p>
+    </div>
+</template>
+<script>
+export default {
+    name:'Todolist'
+}
+</script>
